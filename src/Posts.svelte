47e909675e7726d125f059047e1864112192b668
Eric Lam <ehylam.p@gmail.com>
2020-10-17 15:05:51 +1100

    added fetch WP REST api

---
+++ src/Posts.svelte
@@ -0,0 +1,18 @@
+<script>
+    const fetchData = (async () => {
+        const res = await fetch('http://localhost:8888/tailwind/wp-json/wp/v2/posts')
+        return await res.json()
+    })()
+
+</script>
+
+{#await fetchData}
+<p>...getting posts</p>
+
+{:then data}
+    {#each data as post, i}
+        <div class="post">{post.title.rendered}</div>
+    {/each}
+{:catch error}
+    <p>No posts found.</p>
+{/await}
