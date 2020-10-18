4ea2b9d262fa892abaf93fa9575771458517c4d1
Eric Lam <ehylam.p@gmail.com>
2020-10-18 14:44:51 +1100

    holy moly svelte is cool - animations

--- src/Posts.svelte
+++ src/Posts.svelte
@@ -1,4 +1,5 @@
 <script>
+    import {fly,fade} from 'svelte/transition';
     const fetchData = (async () => {
         const res = await fetch('http://localhost:8888/tailwind/wp-json/wp/v2/posts')
         return await res.json()
@@ -11,7 +12,7 @@
 
 {:then data}
     {#each data as post, i}
-        <div class="post">{post.title.rendered}</div>
+        <div transition:fly={{y:250+1*i,delay: i * 80}} class="post" id={i}>{post.title.rendered}</div>
     {/each}
 {:catch error}
     <p>No posts found.</p>
